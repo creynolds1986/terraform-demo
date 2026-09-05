cc238e9c928df8b0687a2439b1f66523fefa588a
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-09-05 03:27:08 +0100

    Removed vnet

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = var.virtual_network_address_space
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet {
-    name           = "default"
-    address_prefixes = ["10.0.0.0/24"]
-  }
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = var.virtual_network_name
+#   address_space       = var.virtual_network_address_space
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet {
+#     name           = "default"
+#     address_prefixes = ["10.0.0.0/24"]
+#   }
+# }
