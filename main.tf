8529941f7cd7bff1ec2d6ae6a9035f137b674b1e
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-09-05 02:49:17 +0100

    Fixed prefixes error

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   subnet {
     name           = "default"
-    address_prefix = "10.0.0.0/24"
+    address_prefixes = ["10.0.0.0/24"]
   }
 }
