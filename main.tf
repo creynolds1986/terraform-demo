4cba0c6e4db3969bf0a48bfec51cbe4504cefc0d
creynolds1986 <c.reynolds09@hotmail.co.uk>
2026-09-05 02:48:04 +0100

    Added subnet

--- main.tf
+++ main.tf
@@ -23,4 +23,8 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.virtual_network_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  subnet {
+    name           = "default"
+    address_prefix = "10.0.0.0/24"
+  }
 }
