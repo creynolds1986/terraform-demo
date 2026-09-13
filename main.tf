6f7c51fc3bd48cec421bd30dcd4f28adb6fdcb1d
creynolds1986 <269934561+creynolds1986@users.noreply.github.com>
2026-09-13 18:20:27 +0100

    added subnet

--- main.tf
+++ main.tf
@@ -27,4 +27,8 @@ resource "azurerm_virtual_network" "vnet" {
     name             = "default"
     address_prefixes = ["10.0.0.0/24"]
   }
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.1.0/24"]
+  }
 }
